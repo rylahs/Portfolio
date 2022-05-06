453bcfec79c582a38b7092a2623dec2ac3c1d60d
Chan ho Moon <minsonyo@naver.com>
2022-05-06 17:32:28 +0900

    change

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ window.onload = () =>{
     let scrollpos = window.scrollY; 
     const about = document.querySelector("#about > div")
     const tech = document.querySelector("#Tech > div")
-    const awards = document.querySelector("#Current > div") 
+    const current = document.querySelector("#Current > div") 
     const add_class_on_scroll = (dom) => dom.classList.add("come-in")  
     window.addEventListener('scroll', function() { 
       scrollpos = window.scrollY; 
       if (scrollpos >= about.offsetHeight - 200) add_class_on_scroll(about)
       if (scrollpos >= tech.offsetHeight + 300) add_class_on_scroll(tech)
-      if (scrollpos >= awards.offsetHeight + 700) add_class_on_scroll(awards) 
+      if (scrollpos >= current.offsetHeight + 700) add_class_on_scroll(current) 
     }) 
 } 
